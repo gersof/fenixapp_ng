6a6df3df7bb243e3ec06aff0313edd0953636fbc
U-VP\jangulo <jangulo@VPC-JANGULO.bellevue.velocitypartners.net>
2017-08-31 15:56:40 -0500

    Add the connection to firebase and get list bands

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { AngularFireModule } from 'angularfire2';
+import { firebaseConfig } from "../environments/firebase.config";
+import {NgPipesModule} from 'ngx-pipes';
+
+
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 @NgModule({
   declarations: [
@@ -12,8 +20,12 @@ import { LoginComponent } from './components/login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    NgPipesModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig={
+    apiKey: "AIzaSyB5xUXpPYeKK6ZQzbKtRNxfPv8f-V0nm4w",
+    authDomain: "fenixdeacero-999f4.firebaseapp.com",
+    databaseURL: "https://fenixdeacero-999f4.firebaseio.com",
+    projectId: "fenixdeacero-999f4",
+    storageBucket: "fenixdeacero-999f4.appspot.com",
+    messagingSenderId: "149499562090"
+}
