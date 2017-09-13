c70c13e788135cd77577f3335cb0e49adb212b5f
U-VP\jangulo <jangulo@VPC-JANGULO.bellevue.velocitypartners.net>
2017-09-13 17:07:32 -0500

    Install and create step by step form wizard, so i begin create forms

--- src/app/components/masterband/masterband.component.ts
+++ src/app/components/masterband/masterband.component.ts
@@ -12,9 +12,12 @@ export class MasterbandComponent implements OnInit {
     dateFormat: 'yyyy-mm-dd',
     todayBtnTxt:'Hoy'
 };
+private urlimgclient='';
 private NameAlbum:string='';
 private YearAlbum:number=2000;
 private discography: Array<any>=[];
+selectedFiles: FileList;
+
   constructor() { }
 
   ngOnInit() {
@@ -23,6 +26,60 @@ private discography: Array<any>=[];
 
   addAlbumInfoToList(){
     this.discography.push({NameAlbum:this.NameAlbum,YearAlbum:this.YearAlbum});
+  }
+  detectFiles(event) {
+    this.selectedFiles = event.target.files;
+    var file = event.dataTransfer ? event.dataTransfer.files[0] : event.target.files[0];
+
+    var pattern = /image-*/;
+    var reader = new FileReader();
+
+    if (!file.type.match(pattern)) {
+      alert('invalid format');
+      return;
+    }
+
+    reader.onload = this._handleReaderLoaded.bind(this);
+    reader.readAsDataURL(file);
+  }
+  _handleReaderLoaded(e) {
+    var reader = e.target;
+    this.urlimgclient = reader.result;
+  }
+  
+  step2: any = {
+    showNext: true,
+    showPrev: true
+  };
+
+  step3: any = {
+    showSecret: false
+  };
+
+  data: any = {
+    email: 'muk@gmail.com'
+  };
+
+  isCompleted: boolean = false;
+
+  onStep1Next(event) {
+    console.log('Step1 - Next');
+  }
+
+  onStep2Next(event) {
+    console.log('Step2 - Next');
+  }
+
+  onStep3Next(event) {
+    console.log('Step3 - Next');
+  }
+
+  onComplete(event) {
+    this.isCompleted = true;
+  }
+
+  onStepChanged(step) {
+    console.log('Changed to ' + step.title);
   }
 
 }
